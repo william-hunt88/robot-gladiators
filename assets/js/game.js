9c2440629b37df3d1b7c6582883b2fbc418c376d
w.hunt <wfh2d88@gmail.com>
2021-01-02 16:59:02 -0600

    3.5.4

--- assets/js/game.js
+++ assets/js/game.js
@@ -71,12 +71,6 @@ var startGame = function(){
     if (playerInfo.health > 0) {
       // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
       window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
-
-      // reset enemyInfo.health before starting new fight
-      // enemyInfo.health = 50;
-  
-      // use debugger to pause script from running and check what's going on at that moment in the code
-      debugger;
   
       // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
       fight(pickedEnemyObj);
@@ -156,8 +150,17 @@ var randomNumber = function(min, max){
   return value;
 };
 
+
+var getPlayerName =  function(){
+  var name = "";
+  while(name === "" || name === null) {
+    name = prompt("What is your robot's name?");
+  }
+  return name;
+}
+
 var playerInfo = {
-  name: window.prompt("What is your robots name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
